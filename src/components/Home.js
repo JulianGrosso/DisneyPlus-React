c8b237a2c74b12b8e0894834faa18e8458debcbe
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-11-09 14:38:21 -0300

    ADD: Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
+import Viewers from "./Viewers";
 
 // ---- Component ----
 
@@ -8,6 +9,7 @@ const Home = () => {
 	return (
 		<Container>
 			<ImgSlider />
+			<Viewers />
 		</Container>
 	);
 };
