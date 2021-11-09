b32dba227ecd70fbb8708a6e091f46a3d58f3f6e
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-11-09 11:46:12 -0300

    ADD: header

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import styled from "styled-components";
+
+// ---- Component ----
+
+const Home = () => {
+	return <div></div>;
+};
+
+export default Home;
+
+// ---- Style ----
