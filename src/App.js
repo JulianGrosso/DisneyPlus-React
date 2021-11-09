ccccd19b9977a0377a12cea453c75700fd776dc0
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-11-09 16:17:06 -0300

    ADD: Detail page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Routes as Switch,
+	Route,
+	Link,
+} from "react-router-dom";
 import "./App.css";
+import Detail from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<Home />
+			<Router>
+				<Header />
+				<Switch>
+					<Route path="/" element={<Home />} />
+					<Route path="/detail" element={<Detail />} />
+				</Switch>
+			</Router>
 		</>
 	);
 }
